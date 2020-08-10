1069e9f402bc8cc460a9f307aeb3c5f627bea83a
ParkSeongju <parksj914@naver.com>
2020-08-10 14:50:03 +0900

    component-style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import Hello from "./Hello";
-import "./App.css";
 
 function App() {
-    const name = 'react';
-    const style = {
-        backgroundColor : 'black',
-        color: 'aqua',
-        fontSize: 24,
-        padding: '1rem'
-    };
   return (
       <>
-          <Hello/>
-          <Hello/>
-          <Hello/>
-          <Hello/>
-          <div style={style}>{name}</div>
-          <div className="gray-box"></div>
+          <Hello name="react" color="red"/>
       </>
   );
 }
