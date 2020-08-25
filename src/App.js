82a8e4443fd51f117befa506f891c9ab6810964b
ParkSeongju <parksj914@naver.com>
2020-08-26 00:29:49 +0900

    React.memo-필요할때만 랜더링2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CreateUser from "./CreateUser";
 
 function countActiveUsers(users) {
     console.log('확성 사용자 수를 세는중...');
-    return users.filter(user=>user.active).length
+    return users.filter(user=>user.active).length;
 }
 
 function App() {
@@ -18,12 +18,12 @@ function App() {
   const onChange = useCallback(
       e => {
           const {name, value} = e.target;
-          setInputs({
+          setInputs(inputs => ({
               ...inputs,
               [name]: value
-          });
+          }));
       },
-      [inputs]
+      []
   );
 
   const [users, setUsers] = useState([
@@ -55,14 +55,14 @@ function App() {
               username,
               email
           };
-          setUsers(users.concat(user));
+          setUsers(users => users.concat(user));
           setInputs({
               username: '',
               email: ''
           });
           nextId.current += 1;
       },
-      [users,username,email]
+      [username,email]
   );
 
   const onRemove = useCallback(
@@ -70,16 +70,16 @@ function App() {
           //user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
           // = user.id 가 id 인 것을 제거함
           setUsers(
-              users.filter(user=> user.id !== id)
+              users => users.filter(user=> user.id !== id)
           );
       },
-      [users]
+      []
   );
 
   const onToggle = useCallback(
       id => {
           setUsers(
-              users.map(
+              users => users.map(
                   user =>
                       user.id === id
                           ? {...user, active: !user.active}
@@ -87,7 +87,7 @@ function App() {
               )
           )
       },
-      [users]
+      []
   );
 
   const count = useMemo(
