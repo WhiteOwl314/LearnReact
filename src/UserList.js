65d1ee38f28eefc356240aafc84186670342ce0c
ParkSeongju <parksj914@naver.com>
2020-08-26 00:18:46 +0900

    React.memo-필요할때만 랜더링

--- src/UserList.js
+++ src/UserList.js
@@ -1,27 +1,29 @@
 import React, {useEffect} from 'react';
 
-function User({user, onRemove, onToggle}) {
+const User = React.memo(
+    function User({user, onRemove, onToggle}) {
 
-    useEffect(
-        ()=>{
-        });
-    return(
-        <div>
-            <b
-                style={{
-                    cursor: 'pointer',
-                    color: user.active ? 'green' : 'black'
-                }}
-                onClick={() => onToggle(user.id)}
-            >
-                {user.username}
-            </b>
-            &nbsp;
-            <span>({user.email})</span>
-            <button onClick={()=> onRemove(user.id)}>삭제</button>
-        </div>
-    )
-}
+        useEffect(
+            ()=>{
+            });
+        return(
+            <div>
+                <b
+                    style={{
+                        cursor: 'pointer',
+                        color: user.active ? 'green' : 'black'
+                    }}
+                    onClick={() => onToggle(user.id)}
+                >
+                    {user.username}
+                </b>
+                &nbsp;
+                <span>({user.email})</span>
+                <button onClick={()=> onRemove(user.id)}>삭제</button>
+            </div>
+        )
+    }
+);
 
 function UserList({users, onRemove, onToggle}) {
 
@@ -44,4 +46,4 @@ function UserList({users, onRemove, onToggle}) {
 
 }
 
-export default UserList;
+export default React.memo(UserList);
