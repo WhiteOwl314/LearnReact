abaf412ce6064bd7c60834ff524a4ace5a1f0b66
ParkSeongju <parksj914@naver.com>
2020-08-25 18:39:56 +0900

    Component-spread 대신 concat 사용

--- src/App.js
+++ src/App.js
@@ -44,10 +44,7 @@ function App() {
         username,
         email
       };
-      setUsers([
-          ...users,
-          user
-      ]);
+      setUsers([users.concat(user)]);
 
       setInputs({
         username: '',
