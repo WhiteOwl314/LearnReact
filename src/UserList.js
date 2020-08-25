ec5dbc912dac2e31f9a6f3a8102cafc14ecc015a
ParkSeongju <parksj914@naver.com>
2020-08-25 20:08:25 +0900

    Component-상태변화

--- src/UserList.js
+++ src/UserList.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
-function User({user, onRemove}) {
+function User({user, onRemove, onToggle}) {
     return(
         <div>
-            <b>{user.username}</b>
+            <b
+                style={{
+                    cursor: 'pointer',
+                    color: user.active ? 'green' : 'black'
+                }}
+                onClick={() => onToggle(user.id)}
+            >
+                {user.username}
+            </b>
             <span>({user.email})</span>
             <button onClick={()=> onRemove(user.id)}>삭제</button>
         </div>
     )
 }
 
-function UserList({users, onRemove}) {
+function UserList({users, onRemove, onToggle}) {
 
     return (
         <div>
@@ -21,6 +29,7 @@ function UserList({users, onRemove}) {
                             user={user}
                             key={user.id}
                             onRemove={onRemove}
+                            onToggle={onToggle}
                         />
                     )
                 )
