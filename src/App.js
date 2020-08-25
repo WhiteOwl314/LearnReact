a62297365117d6d4909273e0d9c2d661b2d6cd26
ParkSeongju <parksj914@naver.com>
2020-08-25 18:37:11 +0900

    Component-밖에서 모든것 관리2

--- src/App.js
+++ src/App.js
@@ -5,49 +5,56 @@ import CreateUser from "./CreateUser";
 function App() {
 
   const [inputs, setInputs] = useState({
-    username: '',
-    email: ''
+      username: '',
+      email: ''
   });
   const {username, email} = inputs;
 
   const onChange = e => {
-    const {name, value} = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    })
+      const {name, value} = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value
+      })
   };
 
-  const users = [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com'
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com'
-    }
-  ];
-
+  const [users, setUsers] = useState([
+      {
+        id: 1,
+        username: 'velopert',
+        email: 'public.velopert@gmail.com'
+      },
+      {
+        id: 2,
+        username: 'tester',
+        email: 'tester@example.com'
+      },
+      {
+        id: 3,
+        username: 'liz',
+        email: 'liz@example.com'
+      }
+  ]);
   const nextId = useRef(4);
 
   const onCreate = () => {
-    //나중에 구현 할 배열에 항목 추가하는 로직
 
-    setInputs({
-      username: '',
-      email: ''
-    });
+      const user = {
+        id: nextId.current,
+        username,
+        email
+      };
+      setUsers([
+          ...users,
+          user
+      ]);
 
-    console.log(nextId);
-    nextId.current += 1;
+      setInputs({
+        username: '',
+        email: ''
+      });
+
+      nextId.current += 1;
   };
 
   return (
