1aff8c18eb9e1c4ba4860fec941037631af83e4a
ParkSeongju <parksj914@naver.com>
2020-08-25 17:54:04 +0900

    데이터를 props 로 전달

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React from 'react';
 import UserList from "./UserList";
 
 function App() {
+
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+
+  ];
+
   return (
-      <UserList/>
+      <UserList users={users}/>
   );
 }
 
