2e231efb78e96a7e31630c5a511209531c2d2391
ParkSeongju <parksj914@naver.com>
2020-08-25 17:22:03 +0900

    useRef-dom 선택하기

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 function InputSample() {
     const [inputs, setInputs] = useState({
@@ -6,10 +6,11 @@ function InputSample() {
        nickname:''
     });
 
+    const nameInput = useRef();
+
     const {name, nickname} = inputs;
 
     const onChange = (e) => {
-        console.log(e.target);
         const {value, name} = e.target;
         setInputs({
             ...inputs,
@@ -21,13 +22,26 @@ function InputSample() {
         setInputs({
             name:'',
             nickname:'',
-        })
+        });
+        console.log(nameInput.current);
+        nameInput.current.focus();
     };
 
     return(
       <div>
-          <input name="name" placeholder="이름" onChange={onChange} value={name}/>
-          <input name="nickname"placeholder="닉네임" onChange={onChange} value={nickname}/>
+          <input
+              name="name"
+              placeholder="이름"
+              onChange={onChange}
+              value={name}
+              ref = {nameInput}
+          />
+          <input
+              name="nickname"
+              placeholder="닉네임"
+              onChange={onChange}
+              value={nickname}
+          />
           <button onClick={onReset}>초기화</button>
           <div>
               <b>값: </b>
